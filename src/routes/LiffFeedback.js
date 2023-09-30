33c779e7269d7a895e1006d54a1c8874129ebdbe
shun-harutaro <shun_harutaro@protonmail.com>
2023-10-01 07:47:32 +0900

    問い合わせフォーム完成

--- src/routes/LiffFeedback.js
+++ src/routes/LiffFeedback.js
@@ -1,13 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import liff from "@line/liff";
 
 export const LiffFeedback = () => {
+  const [post, setPost] = useState("");
   const initLiff = async () => {
     try {
       await liff.init({ liffId: process.env.REACT_APP_LIFF_ID });
     } catch (err) {
       console.log("Failed to init liff");
     }
+  };
+
+  const handleTextChange = (event) => {
+    const currentVal = event.target.value;
+    setPost(currentVal);
+  };
+
+  const handleSubmit = () => {
+    const postFinal = post;
+    liff.sendMessages([
+      {
+        type: "text",
+        text: "feedback>> " + postFinal,
+      },
+    ]);
   };
 
   useEffect(() => {
@@ -17,8 +33,21 @@ export const LiffFeedback = () => {
   }, []);
 
   return (
-    <div className="main color-page">
-      <h1>準備中！</h1>
+    <div className="main">
+      <div className="block">
+        <p className="goal">
+          <label htmlFor="goal">問い合わせフォーム</label>
+          <textarea
+            name="message"
+            id="goal"
+            value={post}
+            onChange={handleTextChange}
+          />
+        </p>
+        <p className="goal submit">
+          <input onClick={handleSubmit} type="submit" value="feedbackを送信" />
+        </p>
+      </div>
     </div>
   );
 };
