ab14adbb0af9a42481147cc062e8d898787efbf0
shun-harutaro <shun_harutaro@protonmail.com>
2023-10-05 04:48:37 +0900

    時刻設定がhooks経由でできるようになった

--- src/routes/LiffSetTime.js
+++ src/routes/LiffSetTime.js
@@ -2,74 +2,63 @@ import React, { useState, useEffect } from "react";
 import { useLiff } from "../hooks/useLiff";
 import { useLiffInfo } from "../hooks/useLiffInfo";
 import { useLiffMessage } from "../hooks/useLiffMessage";
+import { useAxios } from "../hooks/useAxios";
 import { useLocation } from "react-router-dom";
-import axios from "axios";
-const BASE_URL = process.env.BASE_URL;
 const DEBUG = process.env.DEBUG === "TRUE" ? true : false;
 
 export const LiffSetTime = (props) => {
-  const { liffObject, isLoggedIn } = useLiff();
-  const { sendMessages } = useLiffMessage(liffObject, isLoggedIn);
-  const { idToken } = useLiffInfo(liffObject, isLoggedIn);
   const [log, setLog] = useState("");
+  const [uid, setUid] = useState(props.uid);
+  //const [idToken, setIdToken] = useState("");
   const search = useLocation().search;
   const query = new URLSearchParams(search);
   const targetTime = query.get("target-time");
-
-  const fetchUID = async (idToken) => {
-    const url = `${BASE_URL}/users/inquiry-sub`;
-    try {
-      const res = await axios.get(url, {
-        headers: {
-          Authorization: `Bearer ${idToken}`,
-        },
-      });
-      return res.data.uid;
-    } catch (err) {
-      throw new Error("uid取得に失敗しました" + err.response.data);
-    }
-  };
-
-  const setTargetTime = async (idToken) => {
-    const url = new URL(`${BASE_URL}/target-time/set`);
-    let currentUid = props.uid;
-    try {
-      if (currentUid == null) {
-        currentUid = await fetchUID(idToken);
-        props.setCookieUid(currentUid);
-      }
-      const res = await axios.put(
-        url,
-        {
-          uid: currentUid,
-          targetTime: `2023-06-27T${targetTime}:00+10:00`,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${idToken}`,
-          },
-        },
-      );
-      setLog("time updated " + JSON.stringify(res.data));
-      sendMessages("起床時刻を更新しました");
-    } catch (err) {
-      const errMsg = JSON.stringify(err.response);
-      setLog("failed to update time " + errMsg);
-      sendMessages("時刻更新に失敗しました");
-    } finally {
-      liffObject.closeWindow();
-    }
-  };
+  const { liffObject, isLoggedIn } = useLiff();
+  const { sendMessages } = useLiffMessage(liffObject, isLoggedIn);
+  const { idToken } = useLiffInfo(liffObject, isLoggedIn);
+  const [{ isLoading }, doFetch] = useAxios();
 
   useEffect(() => {
     (async () => {
-      if (idToken) await setTargetTime(idToken);
+      if (uid == null && idToken) {
+        try {
+          const res = await doFetch({
+            method: "get",
+            url: "/users/inquiry-sub",
+            headers: JSON.stringify({ Authorization: `Bearer ${idToken}` }),
+          });
+          setUid(res.uid);
+        } catch (err) {
+          setLog("failed to fetch uid");
+        }
+      }
+      if (uid && idToken) {
+        try {
+          const res = await doFetch({
+            method: "put",
+            url: "/target-time/set",
+            body: JSON.stringify({
+              uid: uid,
+              targetTime: `2023-06-27T${targetTime}:00+10:00`,
+            }),
+            headers: JSON.stringify({ Authorization: `Bearer ${idToken}` }),
+          });
+          setLog("time updated " + JSON.stringify(res));
+          sendMessages("起床時刻を更新しました");
+        } catch (err) {
+          setLog("failed to update time " + JSON.stringify(err));
+          sendMessages("時刻更新に失敗しました");
+        } finally {
+          liffObject.closeWindow();
+        }
+      }
     })();
-  }, [idToken]);
+  }, [idToken, uid]);
 
   return (
     <div>
       {DEBUG && <p>{log}</p>}
+      {DEBUG && isLoading && <p>Loading</p>}
       <h2>登録中</h2>
       <p>ウインドウを閉じないでください！</p>
     </div>
