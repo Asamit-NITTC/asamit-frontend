7d0088cced5491ef35f535f49a155ed629dd68d9
shun-harutaro <shun_harutaro@protonmail.com>
2023-10-06 05:27:47 +0900

    即席ルートページ

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Button } from "../ui/Button";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
     <>
-      <h1>LINEloginテストアプリ</h1>
-      <a href="/liff">LIFF画面</a>
+      <h1>Asamit TOP</h1>
+      <p>ルートページは説明を表示させたい</p>
+      <Link href="/app/home">
+        <Button>Asamitアプリはここから！</Button>
+      </Link>
     </>
   );
 };
