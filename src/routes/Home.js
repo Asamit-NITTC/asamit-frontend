ee013436d8a2701d9a1fe2a63bd19e60d9f1e613
shun-harutaro <shun_harutaro@protonmail.com>
2023-10-11 13:48:44 +0900

    プロフィール画像のUI化、ステータスUIの作成

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,8 +3,9 @@ import { WebAppWrapper } from "../components/WebAppWrapper";
 import { LiffObjectContext } from "../components/LiffObjectProvider";
 import { useLiffInfo } from "../hooks/useLiffInfo";
 import { UserInfoContext } from "../components/UserInfoProvider";
-import { ProfileCard } from "../ui/ProfileCard";
+//import { ProfileCard } from "../ui/ProfileCard";
 import { SummitPending } from "../components/SummitPending";
+import { StatusCard } from "../ui/StatusCard";
 //import { useLocation } from "react-router-dom";
 //import axios from "axios";
 //const BASE_URL = process.env.BASE_URL;
@@ -12,12 +13,12 @@ import { SummitPending } from "../components/SummitPending";
 
 export const Home = () => {
   const { liffObject, isLoggedIn } = useContext(LiffObjectContext);
-  const { userInfo, summitStatus, targetTime } = useContext(UserInfoContext);
+  const { summitStatus, targetTime } = useContext(UserInfoContext);
   const { pictureUrl } = useLiffInfo(liffObject, isLoggedIn);
   return (
     <WebAppWrapper title="ホーム">
       <p>時刻：{targetTime}</p>
-      <ProfileCard pictureUrl={pictureUrl} userInfo={userInfo} />
+      <StatusCard pictureUrl={pictureUrl} targetTime={targetTime} />
       {!summitStatus.affiliation && (
         <div className="default-margin">
           <SummitPending />
