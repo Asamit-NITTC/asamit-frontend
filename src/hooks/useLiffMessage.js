9e04a80fb16b0d9ec0a3a0787b8632c0958e46e2
shun-harutaro <shun_harutaro@protonmail.com>
2023-10-13 16:10:27 +0900

    sendMessagesのno

--- src/hooks/useLiffMessage.js
+++ src/hooks/useLiffMessage.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 export const useLiffMessage = (liffObject, isLoggedIn) => {
   const sendMessages = async (message) => {
     if (!isLoggedIn) return;
@@ -8,6 +10,10 @@ export const useLiffMessage = (liffObject, isLoggedIn) => {
       },
     ]);
   };
+
+  useEffect(() => {
+    if (Object.keys(liffObject).length === 0) return;
+  }, [liffObject]);
 
   return {
     sendMessages,
